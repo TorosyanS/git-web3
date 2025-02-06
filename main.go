0a534101111a0bed589ab4763c5631cbfb1f13f6
Sevak Torosyan <torosyan.sevak@wb.ru>
2025-02-06 21:03:15 +0400

    TASK-105 user created

--- main.go
+++ main.go
@@ -8,8 +8,12 @@ func main() {
 	s := "gopher"
 	fmt.Printf("Hello and welcome, %s!\n", s)
 
-	fmt.Println("Привет!")
-	fmt.Println("Еще раз привет!")
+	fmt.Println("Привет!!!")
+	fmt.Println("Еще раз привет!!!")
+	fmt.Println("Еще раз привет!!!")
+	fmt.Println("Еще раз привет!!!")
+	fmt.Println("Еще раз привет!!!")
+	fmt.Println("Еще раз привет!!!")
 	for i := 1; i <= 5; i++ {
 		fmt.Println("i =", 100/i)
 	}
