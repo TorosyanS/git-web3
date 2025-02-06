16750d565b093f1455b21ca26aefc78aae9e7be1
Sevak Torosyan <torosyan.sevak@wb.ru>
2025-02-06 21:21:39 +0400

    TASK-205-delete-user

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ func main() {
 	fmt.Printf("Hello and welcome, %s!\n", s)
 
 	fmt.Println("Привет!!!")
-	fmt.Println("Еще раз привет!!!")
-	fmt.Println("Еще раз привет!!!")
-	fmt.Println("Еще раз привет!!!")
-	fmt.Println("Еще раз привет!!!")
-	fmt.Println("Еще раз привет!!!")
+	fmt.Println("Еще раз привет!!!!!")
+	fmt.Println("Еще раз привет!!!!!")
+	fmt.Println("Еще раз привет!!!!!")
+	fmt.Println("Еще раз привет!!!!!")
+	fmt.Println("Еще раз привет!!!!!")
 	for i := 1; i <= 5; i++ {
 		fmt.Println("i =", 100/i)
 	}
